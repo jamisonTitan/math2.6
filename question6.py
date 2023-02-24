4d6abbf1aff3b2ba873a7040e4dbf91c8c9ac7fb
Titan Jamison <jamisontitan@gmail.com>
2023-02-23 20:47:03 -0800

    part b and c done

--- question6.py
+++ question6.py
@@ -4,12 +4,24 @@ from scipy.stats import binom
 
 n = 10
 p = 0.6
-k = np.arange(0,n+1)
+x = np.arange(0,n+1)
+x2 = np.arange(0,8)
+x3 = np.arange(8,n+1)
 
-binomialPmf = binom.pmf(k, n, p)
-
-plt.plot(k, binomialPmf, color='blue')
-plt.title(f"Binomial Distribution (n={n}, p={p})")
+plt.plot(x, binom.pmf(x,n,p), color='black')
+plt.fill_between(x2, binom.pmf(x2,n,p) , color="blue")
+plt.fill_between(x3, binom.pmf(x3,n,p), color="orange")
+plt.title("Question 6")
 plt.show()
 
-print(binomialPmf)
+probLessThanEight = 0
+probMoreThanEight = 0
+
+for i in x2:
+    probMoreThanEight += binom.pmf(i,n,p)
+
+for i in x3:
+    probLessThanEight += binom.pmf(i,n,p)
+
+print(probMoreThanEight)
+print(probLessThanEight)
